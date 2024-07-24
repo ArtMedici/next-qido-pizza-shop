ff137b9577ccedafcaa42c1e5e65851d07a19f74
ArtMedici <zcgherty@gmail.com>
2024-07-24 18:32:06 +0500

    ✅ Add: Zustand for Categories, useIntersection in Categories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,6 +30,7 @@ export default function Home() {
 							<div className="flex flex-col gap-16">
 								<ProductsGroupList
 									title="Пиццы"
+									categoryId={1}
 									items={[
 										{
 											id: 1,
@@ -43,7 +44,7 @@ export default function Home() {
 											],
 										},
 										{
-											id: 1,
+											id: 2,
 											name: 'Диабло',
 											imageUrl:
 												'https://media.dodostatic.net/image/r:292x292/11EE7D6149EB101D8727573088FA2EFF.avif',
@@ -54,7 +55,7 @@ export default function Home() {
 											],
 										},
 										{
-											id: 1,
+											id: 3,
 											name: 'Диабло',
 											imageUrl:
 												'https://media.dodostatic.net/image/r:292x292/11EE7D6149EB101D8727573088FA2EFF.avif',
@@ -65,7 +66,7 @@ export default function Home() {
 											],
 										},
 										{
-											id: 1,
+											id: 4,
 											name: 'Диабло',
 											imageUrl:
 												'https://media.dodostatic.net/image/r:292x292/11EE7D6149EB101D8727573088FA2EFF.avif',
@@ -76,7 +77,56 @@ export default function Home() {
 											],
 										},
 									]}
-									categoryId={1}
+								/>
+								<ProductsGroupList
+									title="Комбо"
+									categoryId={2}
+									items={[
+										{
+											id: 1,
+											name: 'Диабло',
+											imageUrl:
+												'https://media.dodostatic.net/image/r:292x292/11EE7D6149EB101D8727573088FA2EFF.avif',
+											items: [
+												{
+													price: 609,
+												},
+											],
+										},
+										{
+											id: 2,
+											name: 'Диабло',
+											imageUrl:
+												'https://media.dodostatic.net/image/r:292x292/11EE7D6149EB101D8727573088FA2EFF.avif',
+											items: [
+												{
+													price: 609,
+												},
+											],
+										},
+										{
+											id: 3,
+											name: 'Диабло',
+											imageUrl:
+												'https://media.dodostatic.net/image/r:292x292/11EE7D6149EB101D8727573088FA2EFF.avif',
+											items: [
+												{
+													price: 609,
+												},
+											],
+										},
+										{
+											id: 4,
+											name: 'Диабло',
+											imageUrl:
+												'https://media.dodostatic.net/image/r:292x292/11EE7D6149EB101D8727573088FA2EFF.avif',
+											items: [
+												{
+													price: 609,
+												},
+											],
+										},
+									]}
 								/>
 							</div>
 						</div>
--- src/components/shared/products-group-list.tsx
+++ src/components/shared/products-group-list.tsx
@@ -1,6 +1,10 @@
+'use client';
+
+import React from 'react';
+import { useIntersection } from 'react-use';
 import { ProductCard, Title } from '@/components/shared';
 import { cn } from '@/lib/utils';
-import React from 'react';
+import { useCategoryStore } from '@/store/category';
 
 interface Props {
 	title: string;
@@ -17,15 +21,31 @@ export const ProductsGroupList: React.FC<Props> = ({
 	categoryId,
 	className,
 }) => {
+	const setActiveCategoryId = useCategoryStore((state) => state.setActiveId);
+	const intersectionRef = React.useRef(null);
+	const intersection = useIntersection(intersectionRef, {
+		threshold: 0.4,
+	});
+
+	React.useEffect(() => {
+		if (intersection?.isIntersecting) {
+			setActiveCategoryId(categoryId);
+		}
+	}, [categoryId, intersection?.isIntersecting, title]);
+
 	return (
-		<div className={className}>
+		<div
+			className={className}
+			id={title}>
 			<Title
 				text={title}
 				size="lg"
 				className="font-extrabold mb-5"
 			/>
 
-			<div className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
+			<div
+				ref={intersectionRef}
+				className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
 				{items.map((product, i) => (
 					<ProductCard
 						key={product.id}
